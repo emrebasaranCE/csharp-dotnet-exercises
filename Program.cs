d08aed068cffbb1deb409c723dff3f315f9cb992
emrebasaranCE <032190053@ogr.uludag.edu.tr>
2024-06-22 18:27:44 +0300

    made some changes

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿int num1 = 50;
+int num2 = 60;
+
+if (num1 > num2)
+{
+    System.Console.WriteLine("Number 1 is bigger then number 2");
+}
+else
+{
+    System.Console.WriteLine("Number 1 is smaller then number 2");
+}
+
+int another_counter = 0;
+int i = 0; 
+while((i < 10) && (another_counter < 10))
+{
+    // System.Console.Write(i + " ");
+    // System.Console.Write(another_counter + " ");
+    i += 1;
+    another_counter++;
+}
