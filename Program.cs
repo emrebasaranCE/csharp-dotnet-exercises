ace9d53d2a30d2c6e96345c81619b734d95b6e32
emrebasaranCE <032190053@ogr.uludag.edu.tr>
2024-06-22 15:59:59 +0300

    Added some variables to the code

--- Program.cs
+++ Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace Program
+{
+    class Test
+    {
+        static void Main(string[] args)
+        {
+            string this_is_string = "Hello world";
+            System.Console.WriteLine($"this_is_string: {this_is_string}");
+
+            byte a = 15;
+            System.Console.WriteLine($"a: {a}");
+
+            sbyte aa = -12;
+            System.Console.WriteLine($"a: {aa}");
+        }
+    }
+}
