1d9d00c5046342f2de633380b37338b366095d41
emrebasaranCE <032190053@ogr.uludag.edu.tr>
2024-06-23 13:49:24 +0300

    first commit after gitignore

--- Program.cs
+++ Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace Program
+{
+    class Test
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                string hello = "1";
+                int int1 = 1;
+
+                double int2 = int1;
+            }
+            catch(Exception ex)
+            {
+                System.Console.WriteLine($"Something wrong happened: {ex.Message}");
+            }
+            finally
+            {
+                System.Console.WriteLine("Process finished.");
+            }
+        }
+    }
+}
