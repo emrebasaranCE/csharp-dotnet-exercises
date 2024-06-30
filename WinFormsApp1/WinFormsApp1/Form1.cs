33c07e5276805fce11c10efdac2fcda5605637d9
emrebasaranCE <032190053@ogr.uludag.edu.tr>
2024-06-30 20:49:58 +0300

    Winform app created...

---
+++ WinFormsApp1/WinFormsApp1/Form1.cs
@@ -0,0 +1,18 @@
+using WinFormsApp1;
+
+namespace WinFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Functions functions = new Functions();
+            functions.getComboFile();
+        }
+    }
+}
---
+++ WinFormsApp1/WinFormsApp1/Functions.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinFormsApp1
+{
+    internal class Functions
+    {
+        public void getComboFile()
+        {
+            OpenFileDialog openfile = new OpenFileDialog();
+            openfile.Title = "Select Combolist";
+            openfile.Filter = "Text Files |*.txt";
+            openfile.RestoreDirectory = true;
+            DialogResult result = openfile.ShowDialog();
+            string file_path_variable = openfile.FileName;
+
+            if (result == DialogResult.OK && !string.IsNullOrEmpty(file_path_variable) && file_path_variable != "filePath")
+            {
+                string file_name = Path.GetFileName(openfile.FileName).Split(".")[0];
+                string file_path = file_path_variable.ToString();
+            }
+        }
+
+
+    }
+}
