5106e43af707503ae387a59b9aaaa536ea0654d3
emrebasaranCE <032190053@ogr.uludag.edu.tr>
2024-07-20 14:02:24 +0300

    Telephone.cs file created.

---
+++ Telephone.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace Telephone_namespcae
+{
+    class Telephone
+    {
+        private string name;
+        private string last_name;
+        private int number;
+
+        public global::System.String Name { get => name; set => name = value; }
+        public global::System.String Last_name { get => last_name; set => last_name = value; }
+        public global::System.Int32 Number { get => number; set => number = value; }
+    }
+}
