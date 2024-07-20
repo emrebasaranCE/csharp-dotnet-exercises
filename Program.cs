81667566aae989d954eecfff28d00cca5e213bc7
emrebasaranCE <032190053@ogr.uludag.edu.tr>
2024-07-20 16:46:42 +0300

    Created default phone users

--- Program.cs
+++ Program.cs
@@ -1,12 +1,29 @@
-﻿using System;
-
-namespace Program
+﻿using TestNamespace;
+namespace ProgramNamespace
 {
     class Prg
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello world");
+            Telephone default_tel_1 = new Telephone("Mike", "Rocky", "5435553412");
+            Telephone default_tel_2 = new Telephone("Ayse", "Kirmizi", "5435553543");
+            Telephone default_tel_3 = new Telephone("Fatma", "Sari", "5435553412");
+            Telephone default_tel_4 = new Telephone("Hayriye", "Yesil", "5435553412");
+            Telephone default_tel_5 = new Telephone("Hasan ", "Irmak", "5435553412");
+
+            // telephone.print_info();
+
+            List<Telephone> telephones = new List<Telephone>();
+            telephones.Add(default_tel_1);
+            telephones.Add(default_tel_2);
+            telephones.Add(default_tel_3);
+            telephones.Add(default_tel_4);
+            telephones.Add(default_tel_5);
+
+            foreach (Telephone tel in telephones)
+            {
+                tel.print_info();
+            }
         }
     }
 }
--- Telephone.cs
+++ Telephone.cs
@@ -1,15 +1,28 @@
-using System;
-
-namespace Telephone_namespcae
+namespace TestNamespace
 {
     class Telephone
     {
         private string name;
         private string last_name;
-        private int number;
+        private string phone_number;
 
-        public global::System.String Name { get => name; set => name = value; }
-        public global::System.String Last_name { get => last_name; set => last_name = value; }
-        public global::System.Int32 Number { get => number; set => number = value; }
+        public string Name { get => name; set => name = value; }
+        public string Last_name { get => last_name; set => last_name = value; }
+        public string Phone_number { get => phone_number; set => phone_number = value; }
+
+        // Constructor method should not have a return type
+        public Telephone(string name, string last_name, string phone_number)
+        {
+            this.name = name;
+            this.last_name = last_name;
+            this.phone_number = phone_number;
+        }
+
+        public void print_info()
+        {
+            Console.WriteLine($"User name: {this.name}");
+            Console.WriteLine($"User last name: {this.last_name}");
+            Console.WriteLine($"User phone number: {this.phone_number}");
+        }
     }
 }
