79c5cb481d75d7dddc2e3b8824f992ca15c0f4a7
emrebasaranCE <032190053@ogr.uludag.edu.tr>
2024-07-25 21:38:09 +0300

    Ortalama hesapla example has been completed.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Ortalama_Hesapla_ns;
+
+
+namespace Program_ns
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Ortalama_Hesapla_ns.Test.not_main();
+        }
+    }
+}
