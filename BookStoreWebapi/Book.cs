604ccf1b7cbd6d745a4343bb1aad0c9ec4caa5e2
emrebasaranCE <032190053@ogr.uludag.edu.tr>
2024-09-11 16:05:54 +0300

    Master 2 | Book.cs file has been created. Also 'Book' class has been created. This class has: int Id string Title int GenreId int PageCount DateTime PublishDate

---
+++ BookStoreWebapi/Book.cs
@@ -0,0 +1,11 @@
+namespace Book
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public required string Title { get; set; }
+        public int GenreId { get; set; }
+        public int PageCount { get; set; }
+        public DateTime PublishDate { get; set; }
+    }
+}
