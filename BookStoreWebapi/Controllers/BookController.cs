d9c701666ee4d50edab861f6ac0826c00c010390
emrebasaranCE <032190053@ogr.uludag.edu.tr>
2024-09-11 15:59:36 +0300

    Master 1 | Project has been created.

---
+++ BookStoreWebapi/Controllers/BookController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookStoreWebapi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]s")]
+    public class BookController : ControllerBase
+    {
+        
+    }
+}
