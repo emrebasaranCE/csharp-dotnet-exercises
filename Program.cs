5b6064740543376389ef178f3d58c4d836d289a3
emrebasaranCE <032190053@ogr.uludag.edu.tr>
2024-09-12 13:16:18 +0300

    Master 3 | In-memory database has been connected and other files created.

--- Program.cs
+++ Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Patika_Linq_Exercise_Repo.DBOperations;
+using Patika_Linq_Exercise_Repo.Entities;
+
+namespace Patika_Linq_Exercise_Repo
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            DataGenerator.Initialize();
+            LinqDbContext _context = new LinqDbContext();
+            var students = _context.Students.ToList<Student>;
+        }
+    }
+}
