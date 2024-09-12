27949461dde982a9ad35e5c31a38a45e7a9d434f
emrebasaranCE <032190053@ogr.uludag.edu.tr>
2024-09-12 12:13:17 +0300

    Master 11 | Booklist has been deleted from BookController.cs (Because we are trying to work with datas thats in the db...)

--- BookStoreWebapi/Controllers/BookController.cs
+++ BookStoreWebapi/Controllers/BookController.cs
@@ -6,30 +6,7 @@ namespace BookStoreWebapi.Controllers
     [Route("[controller]s")]
     public class BookController : ControllerBase
     {
-        private static List<Book> BookList = new List<Book>()
-        {
-            new Book{
-                Id = 1,
-                Title = "The Great Gatsby",
-                GenreId = 1,
-                PageCount = 180,
-                PublishDate = new DateTime(1925, 4, 10)
-            },
-            new Book{
-                Id = 43,
-                Title = "Sherlock Holmes",
-                GenreId = 1,
-                PageCount = 123,
-                PublishDate = new DateTime(1950, 2, 5)
-            },
-            new Book{
-                Id = 3,
-                Title = "Dune",
-                GenreId = 1,
-                PageCount = 450,
-                PublishDate = new DateTime(2000, 12, 15)
-            }
-        };
+        
 
         [HttpGet]
         public List<Book> GetBooks()
