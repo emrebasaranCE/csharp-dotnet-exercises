5699c65bdd0614164b40f0b393f817e7ff71df33
emrebasaranCE <032190053@ogr.uludag.edu.tr>
2024-09-12 08:57:27 +0300

    Master 3 | Book.cs and BookController.cs has been completed for now.

--- BookStoreWebapi/Book.cs
+++ BookStoreWebapi/Book.cs
@@ -1,4 +1,4 @@
-namespace Book
+namespace BookStoreWebapi
 {
     public class Book
     {
--- BookStoreWebapi/Controllers/BookController.cs
+++ BookStoreWebapi/Controllers/BookController.cs
@@ -6,6 +6,37 @@ namespace BookStoreWebapi.Controllers
     [Route("api/[controller]s")]
     public class BookController : ControllerBase
     {
-        
+        private static List<Book> BookList = new List<Book>()
+        {
+            new Book{
+                Id = 1,
+                Title = "The Great Gatsby",
+                GenreId = 1,
+                PageCount = 180,
+                PublishDate = new DateTime(1925, 4, 10)
+            },
+            new Book{
+                Id = 2,
+                Title = "Sherlock Holmes",
+                GenreId = 1,
+                PageCount = 123,
+                PublishDate = new DateTime(1950, 2, 5)
+            },
+            new Book{
+                Id = 3,
+                Title = "Dune",
+                GenreId = 1,
+                PageCount = 450,
+                PublishDate = new DateTime(2000, 12, 15)
+            }
+        };
+
+        [HttpGet]
+        public List<Book> GetBooks()
+        {
+            var bookslist = BookList.OrderBy(x => x.Id).ToList();
+            return bookslist;
+        }
+
     }
 }
