4eb05c30cf4e40e97eee02eab686503f9e8f4a04
emrebasaranCE <032190053@ogr.uludag.edu.tr>
2024-09-12 12:08:01 +0300

    Master 10 | DataGenerator.cs Initialize method and default datas has been added. In Program.cs file, in-memory database connection has been created.

--- BookStoreWebapi/Program.cs
+++ BookStoreWebapi/Program.cs
@@ -2,6 +2,8 @@ using System;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.EntityFrameworkCore;
+using BookStoreWebapi.DBOperations;
 
 namespace BookStoreWebapi
 {
@@ -12,12 +14,24 @@ namespace BookStoreWebapi
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddControllers();
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
 
+            // Add DbContext with InMemory database or another database provider
+            builder.Services.AddDbContext<BookStorageDBContext>(options =>
+                options.UseInMemoryDatabase("BookStoreDatabase")); // In-memory veritabanı kullanılıyor. Yerine SQL kullanabilirsiniz.
+
             var app = builder.Build();
+
+            // Initialize the database with data
+            using (var scope = app.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+
+                // Call DataGenerator to seed the database
+                DataGenerator.Initialize(services);
+            }
 
             // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
