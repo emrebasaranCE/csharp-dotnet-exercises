cba8630227fa64519a1c225567f0b82c24aefcd8
emrebasaranCE <032190053@ogr.uludag.edu.tr>
2024-09-17 14:07:00 +0300

    Master 2 | Example has been completed.

---
+++ Middlewares/HelloMiddleWare.cs
@@ -0,0 +1,26 @@
+namespace Patika_Middleware_Practice_Repo.Middlewares
+{
+    public class HelloMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public HelloMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            Console.WriteLine("Hello world.");
+            await _next.Invoke(context);
+            Console.WriteLine("Bye world!");
+        }
+    }
+    static public class HelloMiddlewareExtension
+    {
+        public static IApplicationBuilder UseHello(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<HelloMiddleware>();
+        }
+    }     
+}
--- Program.cs
+++ Program.cs
@@ -1,7 +1,10 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Patika_Middleware_Practice_Repo.Middlewares;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
@@ -16,6 +19,58 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+// Add custom middleware
+// app.Use(async (context, next) =>
+// {
+//     Console.WriteLine("Middleware 1 started.");
+//     await next.Invoke();
+//     Console.WriteLine("Middleware 1 ended.");
+// });
+
+// app.Use(async (context, next) =>
+// {
+//     Console.WriteLine("Middleware 2 started.");
+//     await next.Invoke();
+//     Console.WriteLine("Middleware 2 ended.");
+// });
+
+// app.Use(async (context, next) =>
+// {
+//     Console.WriteLine("Middleware 3 started.");
+//     await next.Invoke();
+//     Console.WriteLine("Middleware 3 ended.");
+// });
+
+app.UseHello();
+
+app.Use(async (context, next) =>
+{
+    Console.WriteLine("Use middleware started.");
+    await next.Invoke();
+});
+
+// app.map
+app.Map("/example", internalApp =>
+    internalApp.Run(async context =>
+    {
+        Console.WriteLine("/example middleware started.");
+        await context.Response.WriteAsync("/example middleware started.");
+    })
+);
+
+// app.MapWhen();
+app.MapWhen(x => x.Request.Method == "GET", internalApp =>
+{
+    internalApp.Run(async context => 
+    {
+        Console.WriteLine("MapWhen middleware triggered.");
+        await context.Response.WriteAsync("MapWhen middleware started.");
+    });
+});
+
+
+
+// Add the weather forecast endpoint
 var summaries = new[]
 {
     "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
@@ -23,7 +78,7 @@ var summaries = new[]
 
 app.MapGet("/weatherforecast", () =>
 {
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
+    var forecast = Enumerable.Range(1, 5).Select(index =>
         new WeatherForecast
         (
             DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
@@ -36,8 +91,8 @@ app.MapGet("/weatherforecast", () =>
 .WithName("GetWeatherForecast")
 .WithOpenApi();
 
+// Start the app
 app.Run();
-
 
 record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
 {
